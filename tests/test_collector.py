36465a76801933dae24aabc8b5f18d93c65b0ca3
Timothée Mazzucotelli <pawamoy@pm.me>
2021-12-28 19:31:40 +0100

    tests: Add tests (copied from mkdocstrings)

---
+++ tests/test_collector.py
@@ -0,0 +1,25 @@
+"""Tests for the handlers.python module."""
+
+import pytest
+
+from mkdocstrings.handlers.python.collector import CollectionError, PythonCollector
+
+
+def test_collect_missing_module():
+    """Assert error is raised for missing modules."""
+    collector = PythonCollector()
+    with pytest.raises(CollectionError):
+        collector.collect("aaaaaaaa", {})
+
+
+def test_collect_missing_module_item():
+    """Assert error is raised for missing items within existing modules."""
+    collector = PythonCollector()
+    with pytest.raises(CollectionError):
+        collector.collect("mkdocstrings.aaaaaaaa", {})
+
+
+def test_collect_module():
+    """Assert existing module can be collected."""
+    collector = PythonCollector()
+    assert collector.collect("mkdocstrings", {})
