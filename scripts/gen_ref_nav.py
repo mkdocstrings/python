80480b081a46aada65158a7474bf1f1972acc30c
Timothée Mazzucotelli <pawamoy@pm.me>
2023-04-01 16:35:22 +0200

    chore: Template upgrade

--- scripts/gen_ref_nav.py
+++ scripts/gen_ref_nav.py
@@ -17,7 +17,7 @@ for path in sorted(Path("src").rglob("*.py")):
         parts = parts[:-1]
         doc_path = doc_path.with_name("index.md")
         full_doc_path = full_doc_path.with_name("index.md")
-    elif parts[-1] == "__main__":
+    elif parts[-1].startswith("_"):
         continue
 
     nav[parts] = doc_path.as_posix()
@@ -28,5 +28,5 @@ for path in sorted(Path("src").rglob("*.py")):
 
     mkdocs_gen_files.set_edit_path(full_doc_path, ".." / path)
 
-with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
+with mkdocs_gen_files.open("reference/SUMMARY.txt", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
